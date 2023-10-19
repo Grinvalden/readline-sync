27b6e7d6cb3c9b5576c90afde9d9b148f4eda1cb
Grinvalden <1515@dr.com>
2023-10-19 21:11:01 +0300

    !

---
+++ task5.js
@@ -0,0 +1,11 @@
+const calculateDistance = (x1, y1, x2, y2) => { 
+    if(y1 > x1){
+        console.log(y1 - x1)}
+        else{
+        console.log(x1 - y1)} 
+    if(x2 > y2) {
+        console.log(x2 - y2)}
+        else{
+        console.log(y2 - x2)}
+    }
+calculateDistance(5, 10, 46, 46);
